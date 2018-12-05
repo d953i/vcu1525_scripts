d011d5235e97fe4aa7eb196bf2063ce7721d1ab1
d9 <d9@idv-tech.com>
2018-12-04 19:44:42 -0500

    Replace hardcoded strings to variables

--- bitstream_loader/bitstream_loader.sh
+++ bitstream_loader/bitstream_loader.sh
@@ -2,12 +2,25 @@
 
 source /opt/Xilinx/Vivado/2018.2/settings64.sh
 
-#Load bitstream to Card0 using TCL script
-vivado -mode batch -nojournal -nolog -notrace -source card0_keccak.tcl
+CARD0=12809621t094A
+CARD1=13883211t086B
+CARD2=2384343a83439
+CARD4=101432t0239AB
 
-#Remove PCI-E device if already exists
+BITSTREAM0=~/Mining/bitstreams/vcu1525_keccak_21_600.bit
+BITSTREAM1=~/Mining/bitstreams/vcu1525_tribus_3_600.bit
+BITSTREAM2=~/Mining/bitstreams/vcu1525_tribus_3_650.bit
+
+vivado -mode batch -nojournal -nolog -notrace -source loader.tcl -tclargs $CARD0 $BITSTREAM0
+vivado -mode batch -nojournal -nolog -notrace -source loader.tcl -tclargs $CARD1 $BITSTREAM0
+vivado -mode batch -nojournal -nolog -notrace -source loader.tcl -tclargs $CARD2 $BITSTREAM1
+vivado -mode batch -nojournal -nolog -notrace -source loader.tcl -tclargs $CARD3 $BITSTREAM2
+
+#Remove pcie device if already exists and rescan
 sudo sh -c "echo '1' > /sys/bus/pci/devices/0000:05:00.0/remove"
+sudo sh -c "echo '1' > /sys/bus/pci/devices/0000:04:00.0/remove"
+sudo sh -c "echo '1' > /sys/bus/pci/devices/0000:06:00.0/remove"
+sudo sh -c "echo '1' > /sys/bus/pci/devices/0000:07:00.0/remove"
 
-#Rescan PCI-E
-sleep 1
+#sleep 1
 sudo sh -c "echo '1' > /sys/bus/pci/rescan"
--- bitstream_loader/loader.tcl
+++ bitstream_loader/loader.tcl
@@ -1,26 +1,29 @@
 
-#Open hardware device
+set CARD_ID [lindex $argv 0]
+set BITSTREAM [lindex $argv 1]
+
+#Open hardware device.
 open_hw
 
-#Connect to local HW server
+#Connect to local HW server.
 connect_hw_server -url localhost:3121
-current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/12809621t094A]
-set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/12809621t094A]
+current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/$CARD_ID]
+set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/$CARD_ID]
 open_hw_target
 
-#Set current device
+#Set current device.
 current_hw_device [get_hw_devices xcvu9p_0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvu9p_0] 0]
 
-#Load bitstream to device
+#Load bitstream to device.
 set_property PROBES.FILE {} [get_hw_devices xcvu9p_0]
 set_property FULL_PROBES.FILE {} [get_hw_devices xcvu9p_0]
-set_property PROGRAM.FILE {/home/d9/bitstreams/vcu1525_keccak_21_600.bit} [get_hw_devices xcvu9p_0]
+set_property PROGRAM.FILE $BITSTREAM [get_hw_devices xcvu9p_0]
 program_hw_devices [get_hw_devices xcvu9p_0]
 
-#Refresh device
+#Refresh device.
 refresh_hw_device [lindex [get_hw_devices xcvu9p_0] 0]
 
-#Close hardware device
+#Close hardware device.
 close_hw
 
